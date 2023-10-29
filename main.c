fb675b2f632490514000fce774ee1d2bf29377dc
bareeraaa <148616846+bareeraaa@users.noreply.github.com>
2023-10-29 18:59:36 +0500

    Add files via upload

--- main.c
+++ main.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
-#include <stdio.h>
 
 int main() {
-    int rows, i, j;
+    char str1[100], str2[100];
+    int flag = 1;
 
-    printf("Enter the number of rows: ");
-    scanf("%d", &rows);
+    printf("Enter the first string: ");
+    gets(str1);
 
-    for (i = 1; i <= rows; i++) {
-        // Print spaces
-        for (j = 1; j <= rows - i; j++) {
-            printf(" ");
+    printf("Enter the second string: ");
+    gets(str2);
+
+    // Compare the two strings character by character
+    for (int i = 0; str1[i] != '\0' || str2[i] != '\0'; i++) {
+        if (str1[i] != str2[i]) {
+            flag = 0;
+            break;
         }
+    }
 
-        // Print asterisks
-        for (j = 1; j <= i; j++) {
-            printf("* ");
-        }
-
-        printf("\n");
+    if (flag == 1) {
+        printf("The two strings are equal.\n");
+    } else {
+        printf("The two strings are not equal.\n");
     }
 
     return 0;

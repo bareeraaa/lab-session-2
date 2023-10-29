a890d4d8f9172a39ad966e46204b426b41feddea
bareeraaa <148616846+bareeraaa@users.noreply.github.com>
2023-10-29 19:41:30 +0500

    Add files via upload

--- main.c
+++ main.c
@@ -1,24 +1,35 @@
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-
 int main()
 {
-  char str[100];
-  int ctr, ch, i;
-	   printf("Input the string : ");
-       fgets(str, sizeof str, stdin);
-  i=strlen(str);
-  ctr = i;
-  printf("\nThe given sentence is   : %s",str);
-
-  printf("After Case changed the string  is: ");
-  for(i=0; i < ctr; i++)
-  {
-    ch = islower(str[i]) ? toupper(str[i]) : tolower(str[i]);
-    putchar(ch);
-   }
-   printf("\n\n");
-   return 0;
-
+    int arr1[100], n,ctr=0;
+       printf("Input the number of elements to be stored in the array: ");
+       scanf("%d",&n);
+       printf("Input %d elements in the array :\n",n);
+       for(int i=0;i<n;i++)
+            {
+	      printf("element - %d : ",i);
+	      scanf("%d",&arr1[i]);
+	    }
+    printf("\nThe unique elements found in the array are: \n");
+    for(int i=0; i<n; i++)
+    {
+        ctr=0;
+        for(int j=0,k=n; j<k+1; j++)
+        {
+            /*Increment the counter when the seaarch value is duplicate.*/
+            if (i!=j)
+            {
+		       if(arr1[i]==arr1[j])
+              {
+                 ctr++;
+               }
+             }
+        }
+       if(ctr==0)
+        {
+          printf("%d ",arr1[i]);
+        }
+    }
+       printf("\n\n");
+       return 0;
 }
